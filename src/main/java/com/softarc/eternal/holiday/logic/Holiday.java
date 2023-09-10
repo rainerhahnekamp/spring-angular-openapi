af03e9e150029fc9e775fc4ae8eaf851406e02d4
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-10 16:11:37 +0200

    format

--- src/main/java/com/softarc/eternal/holiday/logic/Holiday.java
+++ src/main/java/com/softarc/eternal/holiday/logic/Holiday.java
@@ -3,6 +3,8 @@ package com.softarc.eternal.holiday.logic;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record Holiday(@NotNull Long id, @NotBlank String name, @NotBlank String description, @NotNull Boolean hasCover) {
-
-}
+public record Holiday(
+    @NotNull Long id,
+    @NotBlank String name,
+    @NotBlank String description,
+    @NotNull Boolean hasCover) {}
